6dcb2224db74f6a092e3e74bffcac2e20b6db5c6
eweihermann <eweihermann@outlook.com.br>
2023-03-22 10:47:54 +0100

    first commit

---
+++ src/components/Tweet.tsx
@@ -0,0 +1,43 @@
+import './Tweet.css';
+import { ArrowsClockwise, ChatCircle, Heart } from 'phosphor-react'
+import { Link } from 'react-router-dom'
+
+interface TweetProps {
+    content: string
+}
+
+export function Tweet(props: TweetProps) {
+    return (
+        <Link to="/status" className="tweet">
+            <img src="https://github.com/eweihermann.png" alt="Eduarda Weihermann" />
+
+            <div className="tweet-content">
+                <div className="tweet-content-header">
+                    <strong>Eduarda Weihermann</strong>
+                    <span>@eweihermann</span>
+                </div>
+
+                <p>{props.content}</p>
+
+                <div className="tweet-content-footer">
+                    <button type="button">
+                        <ChatCircle />
+                        20
+                    </button>
+
+                    <button type="button">
+                        <ArrowsClockwise />
+                        20
+                    </button>
+
+                    <button type="button">
+                        <Heart />
+                        20
+                    </button>
+                </div>
+
+            </div>
+
+        </Link>
+    )
+}
